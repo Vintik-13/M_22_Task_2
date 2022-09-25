2cf0dd93efa54b3e8dca39e087e88019e530dca2
Vintik-13 <7flot.13@list.ru>
2022-09-25 19:53:23 +0500

    Добавьте файлы проекта.

---
+++ M_22_Task_2/M_22_Task_2.cpp
@@ -0,0 +1,51 @@
+﻿/*Задание 2. Регистратура
+
+Что нужно сделать
+Напишите программу «Регистратура».
+На вход программе приходят строки с фамилиями или строка Next.
+Пришедшие люди становятся в очередь, а по запросу Next необходимо
+вызвать в регистратуру человека с фамилией, идущей первой в
+лексикографическом порядке (по алфавиту), и вывести его фамилию
+на экран. Фамилии пациентов могут повторяться.
+Каждый запрос (на добавление и вывод) должен работать за O(logn).*/
+
+#include <iostream>
+#include <map>
+#include <string>
+
+const std::string COMAND = "Next";
+
+void addMan(std::multimap<std::string, int>& reg, std::string name) {
+    reg.emplace(name, 0);
+}
+
+bool showMan(std::multimap<std::string, int>& reg, int& pos ) {
+    std::multimap<std::string, int>::iterator it = reg.begin();
+    std::advance(it, pos);
+    if (it != reg.end()) {
+        std::cout << it->first << std::endl;
+        pos++;
+        return true;
+    }
+    else
+        return false;    
+}
+
+
+int main()
+{
+    std::multimap<std::string, int> reg;
+    int pos{ 0 };
+    bool cycle = true;
+    std::string comand;
+    while (cycle) {
+        std::cout << "Enter your last name or \"Next\"";
+        std::getline(std::cin, comand);
+        if (comand != COMAND) {
+            addMan(reg, comand);
+        }
+        else {
+            cycle = showMan(reg, pos);
+        }
+    }    
+}
